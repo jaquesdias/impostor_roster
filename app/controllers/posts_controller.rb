e7fbf268ead2466be412e4c4492d11c5bb14a9fc
Jaques Dias Costa Junior <jaques.junior@bioritmo.com.br>
2018-10-05 17:59:09 -0300

    Get categories and show on select

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @categories = Category.pluck(:name, :id)
     @field = params[:category] if params[:category]
   end
 
